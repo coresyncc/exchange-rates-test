bde35ee7d5a4a8a78cc95bcdb3bddb1e3bba7861
khasan <requiem19990@gmail.com>
2024-11-18 10:19:50 +0400

    tests fix

---
+++ tests/Feature/Example.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use function PHPUnit\Framework\assertTrue;
+
+class Example extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_true_is_true(): void
+    {
+        assertTrue(true);
+    }
+
+}
